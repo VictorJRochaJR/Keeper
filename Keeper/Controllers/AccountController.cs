c2cc1869331ab635efe8a211efac34653cc7bc3b
VictorJRochaJR <vrocha897@gmail.com>
2021-08-04 11:14:26 -0600

    space

--- Keeper/Controllers/AccountController.cs
+++ Keeper/Controllers/AccountController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Keeper.Controllers
 {
     [ApiController]
-    [Route("[controller]")]   
+    [Route("[controller]")]    
     public class AccountController : ControllerBase
     {
         private readonly AccountService _accountService;
